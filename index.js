d8338eb949509e2079ac1f74afbf54990b83f512
danieljuma5 <jumadaniel275@gmail.com>
2023-03-16 14:41:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// scope lab 
+var customerName = "bob";
+const leastFavoriteCustomer = "Daniel";
+
+//toUpperCase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "not Daniel";
+  return leastFavoriteCustomer;
+}
